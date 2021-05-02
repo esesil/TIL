eb30146ccaf56ad2ac2fbe693c3485bae3af422d
esesil <hesies119@gmail.com>
2021-05-02 23:33:18 +0900

    210502 feat(baseball) : create skeleton code

--- java/java-baseball-precourse/src/main/java/Application.java
+++ java/java-baseball-precourse/src/main/java/Application.java
@@ -1,2 +1,11 @@
+import baseball.domain.NumberGenerator;
+
+import java.util.List;
+
 public class Application {
+    public static void main(String[] args) {
+        final NumberGenerator generator = new NumberGenerator();
+        List<Integer> numbers = generator.createRandomNumbers();
+        System.out.println(numbers);
+    }
 }
