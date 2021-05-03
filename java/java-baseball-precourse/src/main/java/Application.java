78ada8859a5f5101f4f2d815062e5c6260aa328a
esesil <hesies119@gmail.com>
2021-05-04 00:23:09 +0900

    210504 feat(baseball) : 기능 완성

--- java/java-baseball-precourse/src/main/java/Application.java
+++ java/java-baseball-precourse/src/main/java/Application.java
@@ -2,13 +2,35 @@ import baseball.domain.Judgement;
 import baseball.domain.NumberGenerator;
 import baseball.domain.Referee;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
+        final NumberGenerator generator = new NumberGenerator();
+        final List<Integer> computer = generator.createRandomNumbers();
+
         final Referee referee = new Referee();
-        final String result = referee.compare(Arrays.asList(7, 8, 9), Arrays.asList(1, 2, 3));
-        System.out.println(result);
+
+        String result ="";
+        while(!result.equals("0 볼 3 스트라이크")){
+            result = referee.compare(computer,askNumbers());
+            System.out.println(result);
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static List<Integer> askNumbers() {
+        System.out.println("숫자를 입력해 주세요 : ");
+        final Scanner scanner = new Scanner(System.in);
+        final String input = scanner.next();
+
+        List<Integer> numbers = new ArrayList<>();
+        for (String number : input.split("")) {
+            numbers.add(Integer.valueOf(number));
+        }
+        return numbers;
     }
 }
