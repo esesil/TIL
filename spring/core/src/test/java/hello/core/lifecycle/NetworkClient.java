b3f2f3ac4a2a63c435d4f9dc81cea90bccb4eecb
esesil <hesies119@gmail.com>
2021-07-23 14:51:17 +0900

    210723 스프링 핵심 원리 기본편(~end)

--- spring/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ spring/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,17 @@
 package hello.core.lifecycle;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -27,4 +31,34 @@ public class NetworkClient {
     public void disconnect(){
         System.out.println("close " + url);
     }
+
+    @PostConstruct
+    public void init() {
+        // property 세팅이 끝나면(의존관계 주입이 끝나면) 호출한다
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+
+
+
+/*    @Override
+    public void afterPropertiesSet() throws Exception {
+        // property 세팅이 끝나면(의존관계 주입이 끝나면) 호출한다
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }*/
 }
